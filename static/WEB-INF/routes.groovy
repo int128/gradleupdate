eacea5e929df1e2903cc3e06efe67c67009965c5
int128 <int128@gmail.com>
2015-03-20 09:59:45 +0900

    Redirect on top page

--- static/WEB-INF/routes.groovy
+++ static/WEB-INF/routes.groovy
@@ -1,3 +1,5 @@
 get '/feed/@filter', forward: '/feed.groovy?filter=@filter', cache: (7 * 24).hours
 
 post '/authorize', forward: '/exchange-oauth-token.groovy'
+
+get '/', redirect: 'https://gradleupdate.github.io/'
