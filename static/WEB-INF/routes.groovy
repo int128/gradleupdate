69bdde52141a25d828e51bfe71c4bf45f4c447d1
int128 <int128@gmail.com>
2015-03-22 02:44:27 +0900

    Add options of CORS

--- static/WEB-INF/routes.groovy
+++ static/WEB-INF/routes.groovy
@@ -1,5 +1,8 @@
 get '/stable', forward: '/get-stable-version.groovy'
+all '/stable', forward: '/util/cors-options.groovy'
+
 get '/rc',     forward: '/get-rc-version.groovy'
+all '/rc',     forward: '/util/cors-options.groovy'
 
 get '/stable/feed', forward: '/feed.groovy?filter=stable', cache: (7 * 24).hours
 get '/rc/feed',     forward: '/feed.groovy?filter=rc', cache: (7 * 24).hours
