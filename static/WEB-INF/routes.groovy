1191b4593c963be14516d92edce81c22d7f9f628
int128 <int128@gmail.com>
2015-03-21 23:32:10 +0900

    Refactor: move util groovlets

--- static/WEB-INF/routes.groovy
+++ static/WEB-INF/routes.groovy
@@ -4,6 +4,6 @@ post '/authorize', forward: '/exchange-oauth-token.groovy'
 
 get  '/repos/@user/@repo', forward: '/get-user-repository.groovy?fullName=@user/@repo'
 post '/repos/@user/@repo', forward: '/save-user-repository.groovy?fullName=@user/@repo'
-all  '/repos/@user/@repo', forward: '/cors-options.groovy'
+all  '/repos/@user/@repo', forward: '/util/cors-options.groovy'
 
 get '/', redirect: 'https://gradleupdate.github.io/'
