88b3293f7bbeee92eee887c7e0644d38fe4a4186
int128 <int128@gmail.com>
2015-03-22 02:34:22 +0900

    Fix URI

--- static/WEB-INF/groovy/internal/check-current.groovy
+++ static/WEB-INF/groovy/internal/check-current.groovy
@@ -1,2 +1,2 @@
 defaultQueue.add(url: '/internal/check-current-stable.groovy')
-defaultQueue.add(url: '/internal/check-current-nightly.groovy')
+defaultQueue.add(url: '/internal/check-current-rc.groovy')
