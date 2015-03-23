818332a366cf605fe224f46912855cce0c8e0cc0
int128 <int128@gmail.com>
2015-03-23 23:25:53 +0900

    Remove unused import

--- src/main/groovy/infrastructure/GradleUpdateWorker.groovy
+++ src/main/groovy/infrastructure/GradleUpdateWorker.groovy
@@ -1,21 +1,19 @@
 package infrastructure
 
-import infrastructure.GitHub
-
 class GradleUpdateWorker {
 
-    final templateRepository = new GitHub()
+    final gitHub = new GitHub()
 
     def bumpTemplate(String version) {
         def branch = "update-gradle-template-$version"
-        templateRepository.removeBranch('gradleupdate/GradleUpdateWorker', branch)
-        templateRepository.createBranch('gradleupdate/GradleUpdateWorker', branch, 'master')
+        gitHub.removeBranch('gradleupdate/GradleUpdateWorker', branch)
+        gitHub.createBranch('gradleupdate/GradleUpdateWorker', branch, 'master')
     }
 
     def bumpUserRepository(String repo) {
         def branch = "update-gradle-of-$repo"
-        templateRepository.removeBranch('gradleupdate/GradleUpdateWorker', branch)
-        templateRepository.createBranch('gradleupdate/GradleUpdateWorker', branch, 'master')
+        gitHub.removeBranch('gradleupdate/GradleUpdateWorker', branch)
+        gitHub.createBranch('gradleupdate/GradleUpdateWorker', branch, 'master')
     }
 
 }
