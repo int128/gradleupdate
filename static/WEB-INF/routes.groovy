e05f14f25c7ffbe08341a86ff642eed27f988dcd
int128 <int128@gmail.com>
2015-03-27 00:56:47 +0900

    Fix routing for trigger

--- static/WEB-INF/routes.groovy
+++ static/WEB-INF/routes.groovy
@@ -9,8 +9,9 @@ get '/rc/feed',     forward: '/feed.groovy?filter=rc', cache: (7 * 24).hours
 
 post '/authorize', forward: '/exchange-oauth-token.groovy'
 
-post '/repos/@user/@repo/gradle/update', forward: '/trigger-update-user-repository.groovy?fullName=@user/@repo'
-all  '/repos/@user/@repo/gradle/update', forward: '/util/cors-options.groovy'
+post '/repos/@user/@repo/gradle/@version',
+    forward: '/trigger-update-user-repository.groovy?fullName=@user/@repo&gradleVersion=@version'
+all  '/repos/@user/@repo/gradle/@version', forward: '/util/cors-options.groovy'
 
 get  '/repos/@user/@repo', forward: '/get-user-repository.groovy?fullName=@user/@repo'
 post '/repos/@user/@repo', forward: '/save-user-repository.groovy?fullName=@user/@repo'
