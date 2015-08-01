cebe6ff54b69d6fef3265d8d0b10425e83f5c303
int128 <int128@gmail.com>
2015-08-01 12:43:48 +0900

    Add credential management tool

---
+++ static/WEB-INF/groovy/internal/admin/credential.groovy
@@ -0,0 +1,22 @@
+import model.Credential
+
+if (request.method == 'POST') {
+    final credential = new Credential()
+    params.each { k, v -> credential[k as String] = v }
+    credential.save()
+    response.sendRedirect(request.requestURL as String)
+} else {
+    html.html {
+        body {
+            p { a href: '/_ah/admin/datastore', '/_ah/admin/datastore' }
+            form(method: 'POST') {
+                ['service', 'clientId', 'clientSecret', 'token'].each { key ->
+                    label(key) {
+                        input type: 'text', name: key
+                    }
+                }
+                input type: 'submit'
+            }
+        }
+    }
+}
