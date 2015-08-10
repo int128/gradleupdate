ad6b2c9fae655eafde7ab14efcee516ff52110e5
int128 <int128@gmail.com>
2015-08-11 01:42:35 +0900

    Add landing page

--- static/WEB-INF/routes.groovy
+++ static/WEB-INF/routes.groovy
@@ -3,5 +3,3 @@ get '/rss', forward: '/rss.groovy', cache: (7 * 24).hours
 post '/authorize', forward: '/exchange-oauth-token.groovy'
 
 post '/webhook', forward: '/receive-github-webhook.groovy'
-
-get '/', redirect: 'https://gradleupdate.github.io/'
