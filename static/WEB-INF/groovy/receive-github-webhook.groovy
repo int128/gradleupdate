28cfcd25c50123f1146247fb3d5551e706e743db
int128 <int128@gmail.com>
2015-08-11 23:53:25 +0900

    Validate signature on received webhook

--- static/WEB-INF/groovy/receive-github-webhook.groovy
+++ static/WEB-INF/groovy/receive-github-webhook.groovy
@@ -1,21 +1,26 @@
 import groovy.json.JsonSlurper
+import infrastructure.GitHubWebhook
 
 final eventType = headers.'X-GitHub-Event'
+final signature = headers.'X-Hub-Signature'
+final payload = request.inputStream.bytes
+assert eventType instanceof String
+assert signature instanceof String
+assert payload
 
-switch (eventType) {
-    case 'watch':
-        final json = new JsonSlurper().parse(request.inputStream)
-        assert json instanceof Map
-        assert json.action == 'started'
-        assert json.sender.login
-        log.info("Queue updating repositories of stargazer ${json.sender.login}")
-        defaultQueue.add(
-                url: '/internal/got-star/0.groovy',
-                params: [stargazer: json.sender.login])
-        break
+assert new GitHubWebhook().validate(signature, payload)
 
-    default:
-        log.warning("Received unknown event type: $eventType")
-        response.sendError(404, 'Event not handled')
-        break
+final json = new JsonSlurper().parse(payload)
+assert json instanceof Map
+
+if (eventType == 'watch' && json.action == 'started') {
+    assert json.sender.login
+    log.info("Queue updating repositories of stargazer ${json.sender.login}")
+    defaultQueue.add(
+            url: '/internal/got-star/0.groovy',
+            params: [stargazer: json.sender.login])
+} else {
+    log.warning("Unknown event type: $eventType")
+    log.info("Payload: ${new String(payload)}")
+    response.sendError(404)
 }
