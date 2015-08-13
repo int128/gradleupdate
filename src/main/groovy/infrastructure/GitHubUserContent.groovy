b471e9800e623f899f521068cf9db3c6a742102a
int128 <int128@gmail.com>
2015-08-13 18:40:54 +0900

    Request If-None-Match for cache content

--- src/main/groovy/infrastructure/GitHubUserContent.groovy
+++ src/main/groovy/infrastructure/GitHubUserContent.groovy
@@ -1,21 +1,56 @@
 package infrastructure
 
+import com.google.appengine.api.memcache.MemcacheService
+import groovy.transform.Canonical
+import groovy.util.logging.Log
+import groovyx.gaelyk.GaelykBindings
 import groovyx.net.http.ContentType
 import groovyx.net.http.HttpURLClient
 import util.HttpURLClientExtension
 
+@Log
+@GaelykBindings
 class GitHubUserContent implements HttpURLClientExtension {
 
     private final HttpURLClient client = new HttpURLClient(url: 'https://raw.githubusercontent.com')
 
     def fetch(String fullName, String branch, String path) {
+        fetch("/$fullName/$branch/$path")
+    }
+
+    def fetch(String fullPath) {
+        assert memcache instanceof MemcacheService
+        def cache = memcache.get(fullPath)
+        assert cache == null || cache instanceof ContentCache
+
         handleHttpResponseException(404: null) {
-            def stream = client.request(
-                    path: "/$fullName/$branch/$path",
-                    contentType: ContentType.BINARY).data
-            assert stream instanceof ByteArrayInputStream
-            stream.bytes
+            def response = client.request(
+                    path: fullPath,
+                    contentType: ContentType.BINARY,
+                    headers: cache ? ['If-None-Match': cache.eTag] : [:])
+            if (response.status == 304) {
+                log.info("Got 304, serving GitHub content from memcache: $fullPath")
+                cache.data
+            } else {
+                log.info("Got $response.status, serving GitHub content from response: $fullPath")
+                def stream = response.data
+                assert stream instanceof ByteArrayInputStream
+                def data = stream.bytes
+                def eTag = response.headers.etag
+                if (eTag instanceof String) {
+                    log.info("Updating cache for GitHub content: $fullPath")
+                    memcache.put(fullPath, new ContentCache(eTag, data))
+                }
+                data
+            }
         }
+    }
+
+    @Canonical
+    static class ContentCache implements Serializable {
+        static final long serialVersionUID = 1L
+        String eTag
+        byte[] data
     }
 
 }
