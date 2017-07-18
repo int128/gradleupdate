a5fdc4edbb424dc363298d6a6d5ab051d7e4b91f
Hidetake Iwata <int128@gmail.com>
2017-07-18 23:38:13 +0900

    Initial commit

---
+++ src/main/java/example/HelloServlet.java
@@ -0,0 +1,16 @@
+package example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloServlet", value = "/")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().append("Hello");
+    }
+}
