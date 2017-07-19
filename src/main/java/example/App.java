4e6316c14b6eb6a1a980d0efd25d8d381910042f
Hidetake Iwata <int128@gmail.com>
2017-07-19 09:47:08 +0900

    Spring Boot 1.5.3

---
+++ src/main/java/example/App.java
@@ -0,0 +1,7 @@
+package example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+}
---
+++ src/main/java/example/HelloController.java
@@ -0,0 +1,12 @@
+package example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    String hello() {
+        return "Hello";
+    }
+}
