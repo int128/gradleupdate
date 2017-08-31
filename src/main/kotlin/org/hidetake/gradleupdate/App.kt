3d141e18f1a19f57a0e8f3363804534d58dd06cb
Hidetake Iwata <int128@gmail.com>
2017-08-31 11:10:08 +0900

    Add request logging

--- src/main/kotlin/org/hidetake/gradleupdate/App.kt
+++ src/main/kotlin/org/hidetake/gradleupdate/App.kt
@@ -1,6 +1,7 @@
 package org.hidetake.gradleupdate
 
 import org.eclipse.egit.github.core.client.GitHubClient
+import org.hidetake.gradleupdate.infrastructure.LoggingGitHubClient
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean
@@ -8,6 +9,6 @@ import org.springframework.context.annotation.Bean
 @SpringBootApplication
 open class App : SpringBootServletInitializer() {
     @Bean
-    open fun gitHubClient(): GitHubClient = GitHubClient().apply {
+    open fun gitHubClient(): GitHubClient = LoggingGitHubClient().apply {
     }
 }
