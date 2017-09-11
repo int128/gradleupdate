7a2ea5f17a77311cf5cd7de3a95404f6652dfc9d
Hidetake Iwata <int128@gmail.com>
2017-09-11 20:14:01 +0900

    Refactor

--- src/main/kotlin/org/hidetake/gradleupdate/app/RepositoryController.kt
+++ src/main/kotlin/org/hidetake/gradleupdate/app/RepositoryController.kt
@@ -4,18 +4,27 @@ import org.hidetake.gradleupdate.domain.RepositoryPath
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.ModelAndView
 
 @Controller
-class StatusController(val service: GradleUpdateService) {
-    @GetMapping("/{owner}/{repo}/status.svg")
+@RequestMapping("/{owner}/{repo}")
+class RepositoryController(val service: GradleUpdateService) {
+    @GetMapping("status.svg")
     fun badge(@PathVariable owner: String, @PathVariable repo: String) =
         BadgeSvg.render(service.getGradleWrapperVersionStatus(RepositoryPath(owner, repo)))
 
-    @GetMapping("/{owner}/{repo}/status")
+    @GetMapping("status")
     fun html(@PathVariable owner: String, @PathVariable repo: String) =
         ModelAndView("status", mapOf(
             "repository" to service.getRepository(RepositoryPath(owner, repo)),
             "pullRequest" to service.findPullRequestForUpdate(RepositoryPath(owner, repo))
         ))
+
+    @PostMapping("update")
+    fun update(@PathVariable owner: String, @PathVariable repo: String): String {
+        service.createPullRequestForLatestGradleWrapper(RepositoryPath(owner, repo))
+        return "redirect:status"
+    }
 }
