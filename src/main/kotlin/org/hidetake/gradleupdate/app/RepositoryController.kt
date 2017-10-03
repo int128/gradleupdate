e14ed44b038b0041c5ae9de173ab61f5cd6b7298
Hidetake Iwata <int128@gmail.com>
2017-10-03 16:23:41 +0900

    Enable edge cache for badges

--- src/main/kotlin/org/hidetake/gradleupdate/app/RepositoryController.kt
+++ src/main/kotlin/org/hidetake/gradleupdate/app/RepositoryController.kt
@@ -1,19 +1,24 @@
 package org.hidetake.gradleupdate.app
 
 import org.hidetake.gradleupdate.domain.RepositoryPath
+import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.ModelAndView
+import javax.servlet.http.HttpServletResponse
 
 @Controller
 @RequestMapping("/{owner}/{repo}")
 class RepositoryController(val service: GradleUpdateService) {
     @GetMapping("status.svg")
-    fun badge(@PathVariable owner: String, @PathVariable repo: String) =
-        BadgeSvg.render(service.getGradleWrapperVersionStatus(RepositoryPath(owner, repo)))
+    fun badge(@PathVariable owner: String, @PathVariable repo: String, response: HttpServletResponse) =
+        BadgeSvg.render(service.getGradleWrapperVersionStatus(RepositoryPath(owner, repo))).also {
+            // https://docs.spring.io/spring-security/site/docs/current/reference/html/headers.html#headers-cache-control
+            response.setHeader(HttpHeaders.CACHE_CONTROL, "public, max-age=30")
+        }
 
     @GetMapping("status")
     fun html(@PathVariable owner: String, @PathVariable repo: String) =
