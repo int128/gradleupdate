ead17d6a581b350a8868f9be00b0287def4eb214
Hidetake Iwata <int128@gmail.com>
2019-02-05 10:29:43 +0900

    Fetch badge sources concurrently to reduce time

--- usecases/get_badge.go
+++ usecases/get_badge.go
@@ -10,6 +10,7 @@ import (
 	"github.com/int128/gradleupdate/usecases/interfaces"
 	"github.com/pkg/errors"
 	"go.uber.org/dig"
+	"golang.org/x/sync/errgroup"
 )
 
 type GetBadge struct {
@@ -22,17 +23,30 @@ type GetBadge struct {
 }
 
 func (usecase *GetBadge) Do(ctx context.Context, id git.RepositoryID) (*usecases.GetBadgeResponse, error) {
-	gradleWrapperProperties, err := usecase.RepositoryRepository.GetFileContent(ctx, id, gradle.WrapperPropertiesPath)
-	if err != nil {
-		return nil, errors.Wrapf(err, "could not get the properties file in %s", id)
-	}
-	currentVersion := gradle.FindWrapperVersion(gradleWrapperProperties)
-	if currentVersion == "" {
-		return nil, errors.Errorf("could not find version from properties file in %s", id)
-	}
-	latestRelease, err := usecase.GradleService.GetCurrentRelease(ctx)
-	if err != nil {
-		return nil, errors.Wrapf(err, "could not get the latest Gradle version")
+	var currentVersion gradle.Version
+	var latestRelease *gradle.Release
+	var eg errgroup.Group
+	eg.Go(func() error {
+		gradleWrapperProperties, err := usecase.RepositoryRepository.GetFileContent(ctx, id, gradle.WrapperPropertiesPath)
+		if err != nil {
+			return errors.Wrapf(err, "error while getting gradle-wrapper.properties in %s", id)
+		}
+		currentVersion = gradle.FindWrapperVersion(gradleWrapperProperties)
+		if currentVersion == "" {
+			return errors.Errorf("error while finding Gradle version in the properties of %s", id)
+		}
+		return nil
+	})
+	eg.Go(func() error {
+		var err error
+		latestRelease, err = usecase.GradleService.GetCurrentRelease(ctx)
+		if err != nil {
+			return errors.Wrapf(err, "error while getting the latest Gradle release")
+		}
+		return nil
+	})
+	if err := eg.Wait(); err != nil {
+		return nil, errors.WithStack(err)
 	}
 
 	if err := usecase.BadgeLastAccessRepository.Save(ctx, domain.BadgeLastAccess{
@@ -41,7 +55,7 @@ func (usecase *GetBadge) Do(ctx context.Context, id git.RepositoryID) (*usecases
 		CurrentVersion: currentVersion,
 		LatestVersion:  latestRelease.Version,
 	}); err != nil {
-		usecase.Logger.Errorf(ctx, "could not save badge access")
+		usecase.Logger.Errorf(ctx, "error while saving badge access")
 	}
 	return &usecases.GetBadgeResponse{
 		CurrentVersion: currentVersion,
